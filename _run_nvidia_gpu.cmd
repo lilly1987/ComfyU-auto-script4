1ae341548ac0e020404a4e831b1973ef010d6fbe
lilly <lilly1987@naver.com>
2025-08-17 15:37:54 +0900

    Update _run_nvidia_gpu.cmd

--- _run_nvidia_gpu.cmd
+++ _run_nvidia_gpu.cmd
@@ -2,6 +2,8 @@ rem Set vram state to: NORMAL_VRAM
 rem  --highvram --cache-classic --cache-lru 128
 :top
 rem ..\python_embeded\python.exe -s ..\ComfyUI\main.py --listen 127.0.0.1,192.168.10.2,192.168.0.6
-..\python_embeded\python.exe -s ..\ComfyUI\main.py --fast --listen 0.0.0.0
+..\python_embeded\python.exe -s ..\ComfyUI\main.py --fast fp16_accumulation --listen 0.0.0.0 
+pause
+
 pause
 goto top
